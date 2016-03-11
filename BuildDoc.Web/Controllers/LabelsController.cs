a41d59d0ec02a14337a07c22387d6d2e78e467c7
idoku <dragon.ice.995@hotmail.com>
2016-03-11 17:23:41 +0800

    init web

---
+++ BuildDoc.Web/Controllers/LabelsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BuildDoc.Web.Controllers
+{
+    public class LabelsController : Controller
+    {
+        //
+        // GET: /Labels/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
